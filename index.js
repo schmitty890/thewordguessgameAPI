de13eb3405f65c609fdf107c2d6b9ca9121dc1ea
Jason Schmitt <schmitty890@gmail.com>
2022-04-01 20:38:05 -1000

    not hitting the backend every 5 mins

--- index.js
+++ index.js
@@ -60,9 +60,9 @@ app.get("/", (req, res) => {
   res.send("test route!");
 });
 
-setInterval(function () {
-  http.get("http://thewordguessgameapi.herokuapp.com/");
-}, 300000); // every 5 minutes (300000)
+// setInterval(function () {
+//   http.get("http://thewordguessgameapi.herokuapp.com/");
+// }, 300000); // every 5 minutes (300000)
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
